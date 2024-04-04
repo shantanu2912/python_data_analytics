643e60112146c2c10a0d69ac1636920aac68422a
Shantanu Sharma <shantanu2912sharma@bbdu.ac.in>
2024-04-04 13:36:12 +0530

    database query and insertion

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 
 from datetime import datetime
 
+# object relational mapping
 Base = declarative_base()
 
 class User(Base):
@@ -23,6 +24,18 @@ class Message(Base):
     message = Column(String(255))
     created_at = Column(DateTime, default=datetime.now)
 
+#utility functions
+def get_db():
+    engine = create_engine('sqlite:///example.db')
+    return sessionmaker(bind=engine)()
+
+def save_to_db(object):
+    db = get_db()      # open database
+    db.add(object)     # insert object
+    db.commit()        # save changes
+    db.close()         # close database
+
+#create database
 if __name__ == "__main__":
     engine = create_engine('sqlite:///example.db')
     Base.metadata.create_all(engine)
